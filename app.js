e685b0ef8583c49d3928dbc963791e669fac02b4
Joshua Lemay <jspades93@gmail.com>
2019-09-26 16:05:59 -0400

    working with async

--- app.js
+++ app.js
@@ -1,36 +1,34 @@
 const express = require('express')
 const morgan = require('morgan')
 const layout = require("./views/layout");
-const Sequelize = require('sequelize')
-const { db } = require('./models');
+const {db} = require('./models/index');
 
 const app = express()
 app.use(morgan('dev'));
 
 app.use(express.static(__dirname + "/public"));
-app.use(express.urlencoded())//bodyParser(extended: true)
+app.use(express.urlencoded()) // bodyParser(extended: true)
 
 app.get("/", (req, res) => {
   res.send(layout(""));
 
 });
 
-db.authenticate().
+db.authenticate({logging: false}).
 then(() => {
   console.log('connected to the database');
 })
+ await db.sync();
+const init = async () => {
+  await db.User.async()
+  await db.Page.async()
 
-//page class
-const Page = db.define("page", {
-  title: Sequelize.STRING,
-  slug: Sequelize.STRING,
-  content: Sequelize.TEXT,
-  status: Sequelize.BOOLEAN
-})
+  const PORT = 1000;
+  app.listen(PORT, () => {
+    console.log(`App listening in port ${PORT}`);
+  }
+)}
 
-//user class
+init();
 
-const PORT = 1000;
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
-});
+
